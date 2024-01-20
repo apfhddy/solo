306f82e18848b6f2a6021c51499517b4783e8bf8
vavogmlfkr3@gmail.com <vavogmlfkr3@gmail.com>
2024-01-20 21:11:48 +0900

    2024-01-20 pm 9:11 update

---
+++ soloProject/src/main/java/detail/Order_Total/Order_Total_Controller.java
@@ -0,0 +1,9 @@
+package detail.Order_Total;
+
+public class Order_Total_Controller {
+	private Order_Total_Service order_Total_Service;
+	
+	public Order_Total_Controller(Order_Total_Service order_Total_Service) {
+		this.order_Total_Service = order_Total_Service;
+	}
+}
