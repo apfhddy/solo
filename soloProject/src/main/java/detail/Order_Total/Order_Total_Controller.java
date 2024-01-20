0052b1834fdcb1cc09f1a723673195e799729bc4
vavogmlfkr3@gmail.com <vavogmlfkr3@gmail.com>
2024-01-20 22:47:55 +0900

    2024-01-20 pm 10:47 update

--- soloProject/src/main/java/detail/Order_Total/Order_Total_Controller.java
+++ soloProject/src/main/java/detail/Order_Total/Order_Total_Controller.java
@@ -1,5 +1,10 @@
 package detail.Order_Total;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,6 +27,22 @@ public class Order_Total_Controller implements ControllerPath{
 	@RequestMapping("order/payment")
 	public String orderPayment() {
 		return ORDER+"orderPayment.jsp";
+	}
+	
+	@RequestMapping("order/pay")
+	public String orderPay(HttpSession session) {
+		
+		List<Map<String,Object>> orderList = (List<Map<String,Object>>)session.getAttribute("orderList");
+		
+		for(Map<String,Object> oneMap : orderList) {
+			System.out.println(oneMap.toString());
+		}
+		
+		
+		
+		
+		
+		return "redirect:/";
 	}
 	
 	
--- soloProject/src/main/java/detail/Orders/Orders_DTO.java
+++ soloProject/src/main/java/detail/Orders/Orders_DTO.java
@@ -4,39 +4,61 @@ public class Orders_DTO {
 	private int orders_no;
 	private int orderTotal_no;
 	private int goodsDetail_no;
+	private int cnt;
+	
 	
 	public Orders_DTO() {}
 
-	public Orders_DTO(int orders_no, int orderTotal_no, int goodsDetail_no) {
+
+	public Orders_DTO(int orders_no, int orderTotal_no, int goodsDetail_no, int cnt) {
 		super();
 		this.orders_no = orders_no;
 		this.orderTotal_no = orderTotal_no;
 		this.goodsDetail_no = goodsDetail_no;
+		this.cnt = cnt;
 	}
+
 
 	public int getOrders_no() {
 		return orders_no;
 	}
 
+
 	public void setOrders_no(int orders_no) {
 		this.orders_no = orders_no;
 	}
+
 
 	public int getOrderTotal_no() {
 		return orderTotal_no;
 	}
 
+
 	public void setOrderTotal_no(int orderTotal_no) {
 		this.orderTotal_no = orderTotal_no;
 	}
+
 
 	public int getGoodsDetail_no() {
 		return goodsDetail_no;
 	}
 
+
 	public void setGoodsDetail_no(int goodsDetail_no) {
 		this.goodsDetail_no = goodsDetail_no;
 	}
+
+
+	public int getCnt() {
+		return cnt;
+	}
+
+
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+
+	
 	
 	
 }
